da65a2034decb79ba8f8ba074fa2f8299c9ca3ee
razvanr91 <razvan.roman91@outlook.com>
2021-12-04 13:18:25 +0000

    Add First Migration

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var provider = builder.Services.BuildServiceProvider();
+var configuration = provider.GetRequiredService<IConfiguration>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
@@ -14,6 +25,21 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+}
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "Something went wrong during migration!");
 }
 
 app.UseHttpsRedirection();
