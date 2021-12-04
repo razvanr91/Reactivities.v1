6a3e43f7cc387345f81381333d49047483ba0963
razvanr91 <razvan.roman91@outlook.com>
2021-12-04 14:31:39 +0000

    Add Semantic Ui to the app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { Header, List } from "semantic-ui-react";
 
 function App() {
 	const [activities, setActivities] = useState([]);
@@ -13,14 +14,13 @@ function App() {
 	}, []);
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<ul>
-					{activities.map((activity: any) => (
-						<li key={activity.id}>{activity.title}</li>
-					))}
-				</ul>
-			</header>
+		<div>
+			<Header as="h2" icon="users" content="Reactivities" />
+			<List>
+				{activities.map((activity: any) => (
+					<List.Item key={activity.id}>{activity.title}</List.Item>
+				))}
+			</List>
 		</div>
 	);
 }
